0a9677f5df9264631da11f69593e87a8db6e2f27
dios2003 <dios2003@mail.ru>
2024-06-04 22:08:48 +0300

    Add files via upload

--- homework1.py
+++ homework1.py
@@ -1,6 +1,6 @@
-num1 = 5
-num2 = 3
-sum = num1+num2
-print(sum)
-result = sum + num1 * num2
-print(result)
+example = 'Компьютеризация'
+print(example[0])
+print(example[-1])
+print(example[8:15])
+print(example[::-1])
+print(example[1::2])
