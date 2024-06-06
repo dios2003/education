cdb8ef21264e9616e1f4e220e9a32af6b80dc823
dios2003 <dios2003@mail.ru>
2024-06-06 06:46:21 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+immutable_var = (1,"Cat", 2, True)
+print(immutable_var)
+# immutable_var[1] = "Dog"
+# Кортежи не поддерживают изменение элементов, так как не поддерживается обращение к элементу кортежа.
+# Изменяться могут только значения внутри элемента.
+mutable_list = [1, 2, 3, 4, 5]
+print(mutable_list)
+mutable_list = [1, 2, 3, 4, 5] + ["String"]
+print(mutable_list)
+mutable_list[0] = "Dog"
+print(mutable_list)
