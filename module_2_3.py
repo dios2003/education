53fb72cc2630dd9f665282666efcafae16920aa6
dios2003 <dios2003@mail.ru>
2024-06-11 07:47:36 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,9 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, 5, 9, 8, 7, 6, 5]
 n = int(0)
-while not (n == len(my_list) + 1):
-    n = n + 1
-    if my_list[n-1] >= 0:
-        print(my_list[n-1])
+while not (n == len(my_list)):
+    if my_list[n] >= 0:
+        print(my_list[n])
+        n = n + 1
         continue
-    elif my_list[n-1] < 0:
+    elif my_list[n] < 0:
         break
