50de67eb686e722d2bc16f5abdda5b7501149494
dios2003 <dios2003@mail.ru>
2024-08-20 06:21:38 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,52 @@
+#"Съедобное, несъедобное":
+
+class Animal:
+    def __init__(self, name, alive=True, fed=False,):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+        return
+
+
+class Plant:
+    def __init__(self, name, edible=False):
+        self.name = name
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
+class Flower(Plant):
+    edible = False
+
+
+class Fruit(Plant):
+    edible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
