d17bd405afb3887b860af42d8d6c666bc88c2cc3
dios2003 <dios2003@mail.ru>
2024-08-21 21:50:10 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,55 @@
+#Задача "Изменять нельзя получать"
+
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, __model, __color, __engine_power):
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+
+    def get_model(self):
+        return self.__model
+
+    def get_horsepower(self):
+        return self.__engine_power
+
+    def get_color(self):
+        return self.__color
+
+    def print_info(self):
+        print(f'Модель: {self.__model}')
+        print(f'Мощность двигателя: {self.__engine_power}')
+        print(f'Цвет: {self.__color}')
+        print(f'Владелец: {self.owner}')
+
+    @staticmethod
+    def set_color(new_color):
+        variants_color = []
+        for i in range(0, len(Vehicle.__COLOR_VARIANTS)):
+            variants_color.append(Vehicle.__COLOR_VARIANTS[i].lower())
+        if new_color.lower() in variants_color:
+            __color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}')
+        return
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+# Проверяем что поменялось
+vehicle1.print_info()
