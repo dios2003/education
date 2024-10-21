e3dc7b9c055aa66edd213f8e2cdd63883d457ff7
dios2003 <dios2003@mail.ru>
2024-10-21 20:40:46 +0300

    Add files via upload

--- module_8_1.py
+++ module_8_1.py
@@ -3,11 +3,13 @@
 def add_everything_up(a, b):
     try:
         result = a+b
-        if isinstance(result, float):
-            result = str(f'{result:.{3}f}')
     except TypeError as exc:
         result = str(a) + str(b)
-    return result
+    else:
+        if isinstance(result, float):
+            result = str(f'{result:.{3}f}')
+    finally:
+        return result
 
 
 print(add_everything_up(123.456, 'строка'))
