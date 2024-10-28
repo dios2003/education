b34d075c22998d90858ec6e8dd03d4e58c278404
dios2003 <dios2003@mail.ru>
2024-10-28 16:21:41 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,11 @@
+# Задача "Вызов разом"
+
+def apply_all_func(int_list, *functions):
+    results = {}
+    for i in range(0, len(functions)):
+        results[functions[i].__name__] = functions[i](int_list)
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
